0f9769a9e688e16ea401d020c7ef435ed90f2220
MazenEldieb <mazeneldeeb12321@gmail.com>
2020-11-29 02:04:23 +0200

    origin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDBgkp0wxDvTU0iRkMnarI5uxAjCEgb4XU",
+    authDomain: "pencil-d9017.firebaseapp.com",
+    databaseURL: "https://pencil-d9017.firebaseio.com",
+    projectId: "pencil-d9017",
+    storageBucket: "pencil-d9017.appspot.com",
+    messagingSenderId: "132128666438",
+    appId: "1:132128666438:web:3879ecad75340027032b47",
+    measurementId: "G-KXMD36MQZ1"
+  }
 };
